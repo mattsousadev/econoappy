eb1c0abcfa51d86ac54d8736bf8a28c07fd78cfc
Matheus Sousa <matheus.sousa.dev@gmail.com>
2022-05-10 22:39:13 -0400

    Feat: Incluir as rotas do recurso 'operation' na api

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+import endpoints
+
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return 'Hello World'
+
+app.include_router(endpoints.endpoints)
 
 if __name__ == '__main__':
     uvicorn.run(app)
